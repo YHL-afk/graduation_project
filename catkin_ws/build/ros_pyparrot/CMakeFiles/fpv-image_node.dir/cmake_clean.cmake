eadac0dfde742cbf1a134a776a74fa499eb9ac09
YuhuiLi <y.li10@student.tue.nl>
2025-12-17 08:50:05 +0100

    Add catkin_ws sources

---
+++ catkin_ws/build/ros_pyparrot/CMakeFiles/fpv-image_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/lyh/catkin_ws/devel/lib/ros_pyparrot/fpv-image_node"
+  "/home/lyh/catkin_ws/devel/lib/ros_pyparrot/fpv-image_node.pdb"
+  "CMakeFiles/fpv-image_node.dir/src/fpv-image.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fpv-image_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws/build/ros_pyparrot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lyh/catkin_ws/src/ros_pyparrot
+# Build directory: /home/lyh/catkin_ws/build/ros_pyparrot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
